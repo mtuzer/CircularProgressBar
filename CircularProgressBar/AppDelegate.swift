54fbf466f74888f3170b67ea6c1372679f6096d7
mtuzer <tuzermert@gmail.com>
2019-08-12 15:10:10 +0300

    The first commit.

--- CircularProgressBar/AppDelegate.swift
+++ CircularProgressBar/AppDelegate.swift
@@ -17,6 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window = window {
+            window.backgroundColor = .white
+            window.rootViewController = ViewController()
+            window.makeKeyAndVisible()
+        }
+        
         return true
     }
 
